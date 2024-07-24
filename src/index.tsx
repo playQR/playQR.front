76311b35f79d7b258aa0f587e97c8bd681905b70
jpark0506 <peterp1@inha.edu>
2024-07-25 01:04:15 +0900

    [Feat] Sentry Error Tracking Platform

--- src/index.tsx
+++ src/index.tsx
@@ -21,13 +21,25 @@ import Comments from './mypage/comments';
 import TicketLogin from './routes/ticketlogin';
 import ConfimationRoute from './routes/confirmationroute';
 import ViewReservation from './ticket/components/viewreservation';
-import { SpeedInsights } from '@vercel/speed-insights/react';
+import * as Sentry from "@sentry/react";
+
 declare global {
   interface Window {
     Kakao: any;
   }
 }
-
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
