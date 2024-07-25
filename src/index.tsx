06c71857b28df9c40e6246c3e2d866b160bb6a17
jpark0506 <peterp1@inha.edu>
2024-07-25 17:07:01 +0900

    [Hotfix] Sentry Logging Integration Modified

--- src/index.tsx
+++ src/index.tsx
@@ -24,16 +24,19 @@ import ConfimationRoute from './routes/confirmationroute';
 import ViewReservation from './ticket/components/viewreservation';
 
 
-declare global {
-  interface Window {
-    Kakao: any;
-  }
-}
+
 Sentry.init({
   dsn: `${process.env.REACT_APP_SENTRY_DSN}`,
   integrations: [
-
-    Sentry.replayIntegration(),
+    Sentry.browserTracingIntegration(),
+    Sentry.browserApiErrorsIntegration({
+      setTimeout: true,
+      setInterval: true,
+      requestAnimationFrame: true,
+      XMLHttpRequest: true,
+      eventTarget: true
+    }),
+    Sentry.captureConsoleIntegration()
   ],
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
@@ -41,6 +44,12 @@ Sentry.init({
   replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
