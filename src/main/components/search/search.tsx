7e6e709d3ef942586fcca38bdacb7400e37b3953
jpark0506 <peterp1@inha.edu>
2024-07-26 14:23:32 +0900

    [Hotfix] 좋아요 오류 해결

--- src/main/components/search/search.tsx
+++ src/main/components/search/search.tsx
@@ -88,7 +88,7 @@ const Search = (props: Props) => {
             
             setIsFetching(false); // 요청 완료 후 isFetching 상태 변경
         }
-    }, [query, page, stop]);
+    }, [query, page, stop, isAuthenticated]);
 
     // 쿼리가 변경될 때 새로운 결과를 가져오기
     useEffect(() => {
